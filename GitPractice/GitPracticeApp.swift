f637fec9a414ec3840614f9c2e61c57ca906121c
SrPika <104371533+RichGN28@users.noreply.github.com>
2026-09-23 21:38:28 -0600

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Ricardo Gómez on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitPracticeTests/GitPracticeTests.swift
@@ -0,0 +1,18 @@
+//
+//  GitPracticeTests.swift
+//  GitPracticeTests
+//
+//  Created by Ricardo Gómez on 23/09/26.
+//
+
+import Testing
+
+struct GitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
